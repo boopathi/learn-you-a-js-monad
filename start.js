e50f4a02be50a676ff867654e1655ff619d74b6b
Boopathi Rajaa <me@boopathi.in>
2016-04-21 21:55:24 +0200

    Add assertions and Just docs

--- start.js
+++ start.js
@@ -1,5 +1,16 @@
 'use strict';
 
+let DEBUG = 1;
+
+/**
+ * Let's start by defining our assert statement to run the test suite
+ * as we learn
+ */
+function assert(a, message) {
+  if (!a) throw new Error('AssertionError: ' + message);
+  else if (DEBUG) console.log('✓ ' + message);
+}
+
 /*
  * Let's start with defining our type system, and define a basic type
  *
@@ -47,6 +58,23 @@ function _div(a, b) {
   num.is(a,b);
   return num(a.value / b.value);
 }
+
+/**
+ * We need a way to check if they are equal,
+ * and we are not going to type check here
+ */
+function _equal(a, b) {
+  return (a.type === b.type) && (a.value === b.value);
+}
+
+/**
+ * Our first assertion
+ * Let's keep our assertions inside an IIFE, so we don't leak things to global
+ */
+(function() {
+  let onePlusTwo = _add(num(1), num(2));
+  assert(_equal(onePlusTwo, num(3)), '1+2=3 native functions');
+})();
 
 /**
  * And here we go,
@@ -109,8 +137,58 @@ const add = func(_add);
 const sub = func(_sub);
 const mul = func(_mul);
 const div = func(_div);
+const equal = func(_equal);
 
-// Enter the Just
+/**
+ * Now is the time to assert our funcs
+ */
+(function() {
+  let one = num(1);
+  let two = num(2);
+  let three = num(3);
+  /**
+   * I know it's a bit idiotic to do this. You're welcome to discontinue.
+   *
+   * And for the patient ones, let's just stop here at 3 numbers
+   * and start doing some operations on them
+   */
+  let onePlusTwo = add.call(one).call(two);
+  assert(equal.call(three).call(onePlusTwo), '1+2=3 with funcs');
+})();
+
+/**
+ * Enter the context
+ * A context is a wrapper for a value. Imagine it to be box or a container
+ *
+ * It can either hold a value or it doesn't
+ * This box either contains a value or it contains nothing (null/undefined)
+ *
+ * The type signature is
+ *
+ * {
+ *   type: 'just' | 'nothing',
+ *   value
+ * }
+ *
+ * So when this box does contain a value, the type is `just`, and when it doesn't
+ * the type is `nothing`
+ *
+ * For example,
+ *   a Just num will be like this
+ *   {
+ *     type: 'just',
+ *     value: {
+ *       type: 'num',
+ *       value: 5
+ *     }
+ *   }
+ *
+ *   and nothing will simply be,
+ *   {
+ *     type: 'nothing'
+ *   }
+ *
+ */
 function Just(a) {
   if (typeof a === 'undefined') return {type: 'nothing'};
   if (a.type === 'just' || a.type === 'nothing') return a;
