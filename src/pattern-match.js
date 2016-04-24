1a9d9b9f9d5e9c01c9b2064e5c938d61d8fa67e8
Boopathi Rajaa <me@boopathi.in>
2016-04-24 17:24:44 +0200

    Add pattern matching that works

--- src/pattern-match.js
+++ src/pattern-match.js
@@ -18,6 +18,64 @@ export function isParam(a) {
   return a === PARAM;
 }
 
+export const atomMatcher = (pattern, value) => isAtom(pattern) && pattern === value;
+export const paramMatcher = pattern => isParam(pattern);
+export const functionMatcher = (pattern, value) => typeof pattern === 'function' && pattern(value);
+export const classMatcher = (pattern, value) => typeof pattern === 'function' && value instanceof pattern;
+
+// export const arrayMatcher = {
+//   match(pattern) {
+//     return Array.isArray(pattern);
+//   },
+//   handle(fn, value, pattern) {
+//
+//     // for (let p in pattern) {
+//     //   let handler = getMatchingHandler(p, fn, value);
+//     //   if (handler )
+//     // }
+//     // I don't why this doesn't work
+//     return match.apply(null, pattern.map(p => [p, fn]))(value);
+//   }
+// };
+
+export const justMatcher = {
+  match(pattern, value) {
+    return isJust(pattern) && isJust(value);
+  },
+  handle(fn, value, pattern) {
+    return match([unwrap(pattern), fn])(unwrap(value));
+  }
+};
+
+export const nothingMatcher = {
+  match(pattern, value) {
+    return isNothing(pattern) && isNothing(value);
+  },
+  handle(fn) {
+    return fn.call();
+  }
+};
+
+export const matchers = [
+  atomMatcher,
+  paramMatcher,
+  justMatcher,
+  nothingMatcher,
+  classMatcher,
+  functionMatcher,
+];
+
+export const defaultHandler = (fn, value) => fn.call(null, value);
+
+export function getMatchingHandler(pattern, fn, value) {
+  for (let m of matchers) {
+    if (typeof m === 'function' && m(pattern, value))
+      return defaultHandler;
+    else if (typeof m === 'object' && m.match(pattern, value))
+      return m.handle;
+  }
+}
+
 export function match(...args) {
   return (a) => {
     for (let arg of args) {
@@ -28,38 +86,16 @@ export function match(...args) {
 
       // if there is a match, return immediately after execution
 
-      // match([0, fn])(0)
-      if (isAtom(pattern)) {
-        if (pattern === a) return fn.call(null, a);
+      const handler = getMatchingHandler(pattern, fn, a);
+
+      if (typeof handler === 'function') {
+        // there is a match and we can handle and return
+        // handlers take signature fn, a, pattern
+        return handler.call(null, fn, a, pattern);
       }
-
-      // match([PARAM, fn])(5)
-      if (isParam(pattern)) {
-        return fn.call(null, a);
-      }
-
-      // match regex ?
-
-      // match fn
-      if (typeof pattern === 'function') {
-        if (pattern(a)) return fn.call(null, a);
-      }
-
-      // match array of patterns to same fn
-      if (Array.isArray(pattern)) {
-        return match(pattern.map(p => [p, fn]));
-      }
-
-      // match([just(PARAM), x => x === 2 ])(just(2))
-      if (isJust(pattern) && isJust(a)) {
-        return match([unwrap(pattern), fn])(unwrap(a));
-      }
-
-      if (isNothing(pattern) && isNothing(a)) {
-        return fn.call();
-      }
-
-      throw new Error('Unhandled Pattern type');
+      // else
+      // continue
     }
+    // throw new Error('Unhandled Pattern type' + args[0], args[1]);
   }
 }
---
+++ test/pattern-match.js
@@ -0,0 +1,24 @@
+import test from 'tape';
+import {just, nothing, isJust, isNothing, unwrap} from '../src/just';
+
+import {match, PARAM} from '../src/pattern-match';
+
+test('simple pattern matching', function(t) {
+  let factorial = match(
+    [0, () => 1],
+    [1, () => 1],
+    [PARAM, n => n * factorial(n-1)]
+  );
+  t.equal( factorial( 5 ), 120 );
+  t.end();
+});
+
+test('just/nothing pattern matching', function(t) {
+  let MayBe42 = match(
+    [just(PARAM), a => a],
+    [nothing(), () => 42]
+  );
+  t.equal( MayBe42( just(42)  ), 42 );
+  t.equal( MayBe42( nothing() ), 42 );
+  t.end();
+});
